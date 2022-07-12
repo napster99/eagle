9beca62df86b4ca46c9a0849b02d3b157be27c47
zhangxiaojian <zhangxj@tuya.com>
2022-07-12 16:36:43 +0800

    feat: update

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import * as ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
 const container: Element = document.getElementById("root");
 const root: ReactDOM.Root = ReactDOM.createRoot(container);
 
-root.render(<h1>Hellopopup</h1>);
+root.render(<h1>Hellopoxxpup</h1>);
