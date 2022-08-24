6ed98215cedca852ad836acea6804e9b3108619d
zhangxiaojian <zhangxj@tuya.com>
2022-08-24 17:33:05 +0800

    feat: udpate

--- src/bg.ts
+++ src/bg.ts
@@ -1,3 +1,55 @@
-const tip: string = "zzzx";
+function test() {
+  alert("我是background");
+}
 
-document.body.innerHTML = "xxxyx";
+var views = chrome.extension.getViews({ type: "popup" });
+console.log("views", views);
+if (views.length > 0) {
+  console.log(views[0].location.href);
+}
+
+function sendMessageToContentScript(message: any, callback: any) {
+  chrome.tabs.query(
+    { active: true, currentWindow: true },
+    function (tabs: any) {
+      chrome.tabs.sendMessage(tabs[0].id, message, function (response: any) {
+        if (callback) callback(response);
+      });
+    }
+  );
+}
+sendMessageToContentScript(
+  { cmd: "test", value: "你好，我是popup！" },
+  function (response: any) {
+    console.log("来自content的回复：" + response);
+  }
+);
+
+// 监听来自content-script的消息
+chrome.runtime.onMessage.addListener(function (
+  request: any,
+  sender: any,
+  sendResponse: any
+) {
+  console.log("收到来自content-script的消息：");
+  console.log(request, sender, sendResponse);
+  sendResponse("我是后台，我已收到你的消息：" + JSON.stringify(request));
+});
+
+// 获取当前选项卡ID
+function getCurrentTabId(callback: any) {
+  chrome.tabs.query(
+    { active: true, currentWindow: true },
+    function (tabs: any) {
+      if (callback) callback(tabs.length ? tabs[0].id : null);
+    }
+  );
+}
+
+getCurrentTabId((tabId: any) => {
+  chrome.tabs.executeScript(tabId, {
+    code: `document.body.innerHTML = ${tabId}`,
+  });
+});
+
+console.log(chrome);
--- src/content.ts
+++ src/content.ts
@@ -1,97 +1,38 @@
-let start_time = +new Date();
-
+// declare const webVitals: any;
 declare const chrome: any;
-
-// window.onload = function () {
-// document.body.innerHTML = "aaa";
-// alert(66);
+// var script = document.createElement("script");
+// script.src = "https://unpkg.com/web-vitals/dist/web-vitals.iife.js";
+// script.onload = function () {
+//   // When loading `web-vitals` using a classic script, all the public
+//   // methods can be found on the `webVitals` global namespace.
+//   setTimeout(() => {
+//     console.log("webV1itals", webVitals);
+//   }, 5000);
+//   webVitals.getCLS(console.log);
+//   webVitals.getFID(console.log);
+//   webVitals.getLCP(console.log);
 // };
+// document.head.appendChild(script);
 
-function getTiming() {
-  setTimeout(() => {
-    let t = window.performance.timing;
-    let performanceInfo = [
-      {
-        key: "Redirect",
-        desc: "网页重定向耗时",
-        "value(ms)": t.redirectEnd - t.redirectStart,
-      },
-      {
-        key: "AppCache",
-        desc: "检查本地缓存的耗时",
-        "value(ms)": t.domainLookupStart - t.fetchStart,
-      },
-      {
-        key: "DNS",
-        desc: "DNS查询的耗时",
-        "value(ms)": t.domainLookupEnd - t.domainLookupStart,
-      },
-      {
-        key: "TCP",
-        desc: "TCP链接的耗时",
-        "value(ms)": t.connectEnd - t.connectStart,
-      },
-      {
-        key: "Waiting(TTFB)",
-        desc: "从客户端发起请求到接收响应的时间",
-        "value(ms)": t.responseStart - t.requestStart,
-      },
-      {
-        key: "Content Download",
-        desc: "下载服务端返回数据的时间",
-        "value(ms)": t.responseEnd - t.responseStart,
-      },
-      {
-        key: "HTTP Total Time",
-        desc: "http请求总耗时",
-        "value(ms)": t.responseEnd - t.requestStart,
-      },
-      {
-        key: "First Time",
-        desc: "首包时间",
-        "value(ms)": t.responseStart - t.domainLookupStart,
-      },
-      {
-        key: "White screen time",
-        desc: "白屏时间",
-        "value(ms)": t.responseEnd - t.fetchStart,
-      },
-      {
-        key: "Time to Interactive(TTI)",
-        desc: "首次可交互时间",
-        "value(ms)": t.domInteractive - t.fetchStart,
-      },
-      {
-        key: "DOM Parsing",
-        desc: "DOM 解析耗时",
-        "value(ms)": t.domInteractive - t.responseEnd,
-      },
-      {
-        key: "DOMContentLoaded",
-        desc: "DOM 加载完成的时间",
-        "value(ms)": t.domInteractive - t.navigationStart,
-      },
-      {
-        key: "Loaded",
-        desc: "页面load的总耗时",
-        "value(ms)": t.loadEventEnd - t.navigationStart,
-      },
-    ];
-    console.log("111111", window.location.href);
-    console.table(performanceInfo);
-  }, 0);
-}
+document.body.style.border = "10px solid blue";
 
-window.addEventListener("load", getTiming, false);
+var bg = chrome.extension.getBackgroundPage();
 
-window.addEventListener(
-  "load",
-  function () {
-    var head = document.getElementsByTagName("head").item(0);
-    document.body.style.border = "10px solid red";
-    const script = document.createElement("script");
-    script.setAttribute("src", "cur.bundle.js");
-    head.appendChild(script);
-  },
-  false
+console.log(bg);
+
+chrome.runtime.onMessage.addListener(function (
+  request: any,
+  sender: any,
+  sendResponse: any
+) {
+  // console.log(sender.tab ?"from a content script:" + sender.tab.url :"from the extension");
+  if (request.cmd == "test") alert(request.value);
+  sendResponse("我收到了你的消息！");
+});
+
+chrome.runtime.sendMessage(
+  { greeting: "你好，我是content-script呀，我主动发消息给后台！" },
+  function (response: any) {
+    console.log("收到来自后台的回复：" + response);
+  }
 );
